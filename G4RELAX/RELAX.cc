3d03fad4c53b7513898c2bf366d6b08a75adef91
Nickolas Upole <nickolas@uchicago.edu>
2018-01-10 15:16:37 -0600

    Updated RELAX.cc for full simulation chain

--- G4RELAX/RELAX.cc
+++ G4RELAX/RELAX.cc
@@ -1,8 +1,8 @@
 ///////////////////////////
 // Nickolas Upole        //
 // University of Chicago //
-// Winter 2016           //
-// G4XCD.cc              //
+// Winter 2018           //
+// RELAX.cc              //
 ///////////////////////////
 
 #include "G4RunManager.hh"
@@ -14,31 +14,54 @@
 #include "RELAXDetectorConstruction.hh"
 #include "RELAXPhysicsList.hh"
 #include "RELAXPrimaryGeneratorAction.hh"
+#include "RELAXRunAction.hh"
+#include "RELAXEventAction.hh"
+#include "RELAXSteppingAction.hh"
+#include "RELAXStackingAction.hh"
+#include "RELAXAnalysisManager.hh"
 
 int main(int argc, char** argv)
 {
-    // Construct the Run Manager/Analysis Manager
+    ////////////////////////////////////////////
+    // Construct Run Manager/Analysis Manager //
+    ////////////////////////////////////////////
     G4RunManager* pRunManager = new G4RunManager();
+    RELAXAnalysisManager* pRELAXAnalysisManager = new RELAXAnalysisManager();
 
-    // Set Initialization Classes
+    ////////////////////////////////
+    // Set Initialization Classes //
+    ////////////////////////////////
     pRunManager->SetUserInitialization(new RELAXDetectorConstruction());
     pRunManager->SetUserInitialization(new RELAXPhysicsList());
 
-    // Set Action Classes
+    ////////////////////////
+    // Set Action Classes //
+    ////////////////////////
     pRunManager->SetUserAction(new RELAXPrimaryGeneratorAction());
+    pRunManager->SetUserAction(new RELAXRunAction(pRELAXAnalysisManager));
+    pRunManager->SetUserAction(new RELAXEventAction(pRELAXAnalysisManager));
+    pRunManager->SetUserAction(new RELAXSteppingAction(pRELAXAnalysisManager));
+    pRunManager->SetUserAction(new RELAXStackingAction());
     pRunManager->Initialize();
 
-    // Initialize Visualization
+    /////////////////////////////////////
+    // Construct Visualization Manager //
+    /////////////////////////////////////
     G4VisManager* pVisualManager = new G4VisExecutive();
     pVisualManager->Initialize();
 
-    // Define UI Session
-    G4UIsession* pUISession = new G4UIterminal(new G4UItcsh());
+    //////////////////////////////////
+    // Construct UI Manager/Session //
+    //////////////////////////////////
     G4UImanager* pUIManager = G4UImanager::GetUIpointer();
+    G4UIsession* pUISession = new G4UIterminal(new G4UItcsh());
     pUISession->SessionStart();
 
-    // Terminate Job
+    ///////////////////
+    // Terminate Job //
+    ///////////////////
     delete pUISession;
+    delete RELAXAnalysisManager;
     delete pVisualManager;
     delete pRunManager;
 
